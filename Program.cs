cfa9efb42ddf496a78389f483561c92dee9a4fb6
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-05-06 22:17:09 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Web;
+
+class Program
+{
+    const int __PORT_WRITE = 1000;
+    const int __PORT_READ = 1001;
+    const string __SUBCRIBE_IN = "__TRANSLATE_IN";
+    const string __SUBCRIBE_OUT = "__TRANSLATE_OUT";
+    static RedisBase m_subcriber;
+    static bool __running = true;
+
+    static void TRANSLATE_TEXT_GOOGLE_01(string requestId, string input)
+    {
+        string languagePair = "en|vi"; //en|vi or en|ja
+        Encoding encoding = System.Text.Encoding.UTF7;
+        string result = string.Empty;
+        string url = string.Format("http://www.google.com/translate_t?hl=en&ie=UTF8&text={0}&langpair={1}", input, languagePair);
+        string s = string.Empty;
+        using (WebClient webClient = new WebClient())
+        {
+            webClient.Encoding = encoding;
+            s = webClient.DownloadString(url);
+        }
+        int p = s.IndexOf("id=result_box");
+        if (p > 0)
+            s = s.Substring(p, s.Length - p);
+        p = s.IndexOf("</span>");
+        if (p > 0)
+        {
+            s = s.Substring(0, p);
+            p = s.IndexOf(@"'"">");
+            if (p > 0)
+                result = s.Substring(p + 3, s.Length - (p + 3));
+        }
+
+        var redis = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_WRITE, __PORT_WRITE));
+        var cmd = COMMANDS.TRANSLATE_TEXT_GOOGLE_01.ToString();
+        cmd = cmd.Substring(0, cmd.Length - 3);
+        redis.HSET("_TRANSLATE", input, result);
+        redis.ReplyRequest(requestId, cmd, 1, "01", input, result);
+    }
+
+    static void __executeTaskHttp(Tuple<string, string, string> data)
+    {
+        string requestId = data.Item1, cmd = data.Item2, input = data.Item3;
+        switch (cmd) {
+            case "TRANSLATE_TEXT_GOOGLE_01":
+                TRANSLATE_TEXT_GOOGLE_01(requestId, input);
+                break;
+        }
+    }
+
+    #region [ MAIN ]
+
+    static void __executeTaskTcp(Tuple<string, byte[]> data)
+    {
+        if (data == null || data.Item2 == null || data.Item2.Length < 39) return;
+        var buf = data.Item2;
+        string requestId = Encoding.ASCII.GetString(buf, 0, 36);
+        var cmd = ((COMMANDS)((int)buf[36])).ToString();
+        string text = Encoding.UTF8.GetString(buf, 37, buf.Length - 37);
+        __executeTaskHttp(new Tuple<string, string, string>(requestId, cmd, text));
+    }
+
+    static WebServer _http;
+    static int _http_port = __freeTcpPort();
+    static void __startApp()
+    {
+        string uri = string.Format("http://127.0.0.1:{0}/", _http_port);
+        _http = new WebServer(__executeTaskHttp);
+        _http.Start(uri);
+        m_subcriber = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_SUBCRIBE, __PORT_READ));
+        m_subcriber.PSUBSCRIBE(__SUBCRIBE_IN);
+        var bs = new List<byte>();
+        while (__running)
+        {
+            if (!m_subcriber.m_stream.DataAvailable)
+            {
+                if (bs.Count > 0)
+                {
+                    var buf = m_subcriber.__getBodyPublish(bs.ToArray(), __SUBCRIBE_IN);
+                    bs.Clear();
+                    if (buf != null)
+                        new Thread(new ParameterizedThreadStart((o) =>
+                        __executeTaskTcp((Tuple<string, byte[]>)o))).Start(buf);
+                }
+                Thread.Sleep(100);
+                continue;
+            }
+            byte b = (byte)m_subcriber.m_stream.ReadByte();
+            bs.Add(b);
+        }
+    }
+
+    static void __stopApp()
+    {
+        __running = false;
+        _http.Stop();
+    }
+
+    // FOR SETTING OF WINDOWS SERVICE
+
+    static Thread __threadWS = null;
+    static void Main(string[] args)
+    {
+        if (Environment.UserInteractive)
+        {
+            Console.Title = string.Format("{0} - {1}", __SUBCRIBE_IN, _http_port);
+            StartOnConsoleApp(args);
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey(true);
+            Stop();
+        }
+        else using (var service = new MyService())
+                ServiceBase.Run(service);
+    }
+
+    public static void StartOnConsoleApp(string[] args) => __startApp();
+    public static void StartOnWindowService(string[] args)
+    {
+        __threadWS = new Thread(new ThreadStart(() => __startApp()));
+        __threadWS.IsBackground = true;
+        __threadWS.Start();
+    }
+
+    public static void Stop()
+    {
+        __stopApp();
+        if (__threadWS != null) __threadWS.Abort();
+    }
+    static int __freeTcpPort()
+    {
+        var l = new System.Net.Sockets.TcpListener(IPAddress.Loopback, 0);
+        l.Start();
+        int port = ((IPEndPoint)l.LocalEndpoint).Port;
+        l.Stop();
+        return port;
+    }
+    #endregion;
+}
+
